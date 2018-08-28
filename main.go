4d41a2e3bd8a7b0fc876ee23df3764f504a646fd
Iszak Bryan <38895+iszak@users.noreply.github.com>
2018-08-28 10:05:30 +0100

    Use version tag in cli configuration
    This automatically adds support for the -v and --version flags
    however it does not add a sub-command, so we must still keep it
    to maintain backwards compatibility.

--- main.go
+++ main.go
@@ -16,18 +16,6 @@ func Run(args []string) int {
 }
 
 func RunCustom(args []string, commands map[string]cli.CommandFactory) int {
-	// Get the command line args. We shortcut "--version" and "-v" to
-	// just show the version.
-	for _, arg := range args {
-		if arg == "-v" || arg == "-version" || arg == "--version" {
-			newArgs := make([]string, len(args)+1)
-			newArgs[0] = "version"
-			copy(newArgs[1:], args)
-			args = newArgs
-			break
-		}
-	}
-
 	// Build the commands to include in the help now.
 	commandsInclude := make([]string, 0, len(commands))
 	for k := range commands {
@@ -38,6 +26,7 @@ func RunCustom(args []string, commands map[string]cli.CommandFactory) int {
 		Args:     args,
 		Commands: commands,
 		HelpFunc: cli.FilteredHelpFunc(commandsInclude, cli.BasicHelpFunc("libra")),
+		Version:  VersionString(),
 	}
 
 	exitCode, err := cli.Run()
