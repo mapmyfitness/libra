e6a5dd368fb77e7a5e316fd1d0b01298146787ed
Iszak Bryan <38895+iszak@users.noreply.github.com>
2018-08-28 13:57:57 +0100

    Use NewCLI constructor and set app name and autocomplete
    Using the constructor removes more boilerplate code, includes all
    commands in the help by default - which we were already doing.

--- main.go
+++ main.go
@@ -16,20 +16,11 @@ func Run(args []string) int {
 }
 
 func RunCustom(args []string, commands map[string]cli.CommandFactory) int {
-	// Build the commands to include in the help now.
-	commandsInclude := make([]string, 0, len(commands))
-	for k := range commands {
-		commandsInclude = append(commandsInclude, k)
-	}
+	c := cli.NewCLI("libra", VersionString())
+	c.Args = args
+	c.Commands = commands
 
-	cli := &cli.CLI{
-		Args:     args,
-		Commands: commands,
-		HelpFunc: cli.FilteredHelpFunc(commandsInclude, cli.BasicHelpFunc("libra")),
-		Version:  VersionString(),
-	}
-
-	exitCode, err := cli.Run()
+	exitCode, err := c.Run()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error executing CLI: %s\n", err.Error())
 		return 1
